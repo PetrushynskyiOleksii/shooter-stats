21ba1d9b97642deee28bd688b6bc1ed7c012ba3a
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-09-01 20:13:55 +0300

    Configurate postgres database

--- src/settings/config.py
+++ src/settings/config.py
@@ -9,6 +9,8 @@ class BaseConfig(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET', 'default_secret_key')
+    DEFAULT_URI = 'postgresql://shooter:shooter@localhost/shooterstats'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', DEFAULT_URI)
 
 
 class DevelopmentConfig(BaseConfig):
