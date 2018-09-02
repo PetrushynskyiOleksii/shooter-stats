1bf0df32a48a7125de827802d9171feab99d7f74
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-09-02 18:32:57 +0300

    Delete mod model

--- src/app/models/shooter.py
+++ src/app/models/shooter.py
@@ -3,13 +3,6 @@
 from . import db, BaseManager
 
 
-mods = db.Table(
-    'mods',
-    db.Column('game_mod_id', db.Integer, db.ForeignKey('game_mod.id'), primary_key=True),
-    db.Column('server_id', db.Integer, db.ForeignKey('server.id'), primary_key=True)
-)
-
-
 class Server(db.Model, BaseManager):
     """Server database representation."""
 
@@ -18,7 +11,6 @@ class Server(db.Model, BaseManager):
     id = db.Column(db.Integer, primary_key=True)
     endpoint = db.Column(db.String(128), nullable=False, unique=True)
     title = db.Column(db.String(64), nullable=False)
-    mods = db.relationship('GameMod', secondary=mods, lazy='subquery')
 
     def __init__(self, data):
         """Post model constructor."""
@@ -38,30 +30,6 @@ class Server(db.Model, BaseManager):
     def get_server(id):
         """Retrieve particular server instance."""
         return Server.query.get(id)
-
-
-class GameMod(db.Model, BaseManager):
-    """Server database representation."""
-
-    __tablename__ = 'game_mod'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(10), nullable=False, unique=True)
-    description = db.Column(db.Text, nullable=False)
-
-    def __init__(self, data):
-        """Game mods model constructor."""
-        self.title = data.get('title')
-        self.description = data.get('description')
-
-    @staticmethod
-    def get_gamemod(id):
-        """Retrieve particular game mod instance."""
-        return GameMod.query.get(id)
-
-    def __repr__(self):
-        """Return game mod instance as a string."""
-        return f'{self.title}'
 
 
 class Match(db.Model, BaseManager):
