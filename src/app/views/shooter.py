5f118a65fb912a7bba7ca8a020b169973b1ee3b4
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-09-02 19:01:42 +0300

    Add endpoint for create server instances

--- src/app/views/shooter.py
+++ src/app/views/shooter.py
@@ -1,5 +1,33 @@
 """Collection of game statistic endpoints."""
 
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
+
+from app.models.schemes import server_schema
+from app.models.shooter import Server
 
 shooter_api = Blueprint('shooter', __name__)
+
+
+@shooter_api.route('/', methods=['POST'])
+def create_server():
+    """Create new server instance."""
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'error': 'No input data provided.'}), 400
+    # Validate and deserialize input
+    try:
+        data = server_schema.load(json_data).data
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    server = Server.query.filter_by(endpoint=data.get('endpoint')).first()
+    if server:
+        return jsonify({'error': 'This endpoint already exists.'}), 400
+
+    # Create a new server instance
+    server = Server(data)
+    server.save()
+    response = server_schema.dump(server).data
+
+    return jsonify(response), 201
