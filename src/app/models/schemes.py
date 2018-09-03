ef81da301f7fa8cb6dc19cbfd86380840781dedd
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-09-03 20:26:10 +0300

    Create marshmallow serializer for match model

--- src/app/models/schemes.py
+++ src/app/models/schemes.py
@@ -4,7 +4,7 @@ from marshmallow import Schema, fields
 
 
 class ServerSchema(Schema):
-    """Schema for server JSON representation."""
+    """Serializer schema for server JSON representation."""
 
     id = fields.Integer(dump_only=True)  # read only field
     endpoint = fields.Str(required=True)  # TODO: validation
@@ -13,3 +13,19 @@ class ServerSchema(Schema):
 
 server_schema = ServerSchema()
 servers_schema = ServerSchema(many=True)
+
+
+class MatchServer(Schema):
+    """Serializer schema for match JSON representation."""
+
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    server = fields.Nested(ServerSchema, required=True)
+    start_time = fields.DateTime(required=True)
+    end_time = fields.DateTime(required=True)
+
+    # scoreboard = db.relationship('MatchPlayer', backref='match', lazy=True)
+    # TODO: preload elapsed time
+
+
+matches_schema = MatchServer(many=True)
