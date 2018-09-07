92a0b66727df83eaa2c37f5647155a8d4fff27fd
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-09-07 20:32:25 +0300

    Switch queries to pure sql alchemy

--- src/app/views/servers.py
+++ src/app/views/servers.py
@@ -10,44 +10,45 @@ from app.models.schemes import servers_schema, server_schema, players_schema
 from . import shooter_api
 
 
-@shooter_api.route('/', methods=['POST', 'GET'])
-def create_or_list_servers():
-    """Create new server instance or return servers list."""
-    if request.method == 'GET':
-        # Query all existing servers
-        servers = Server.query.all()
-        # Serialize the queryset
-        response = servers_schema.dump(servers)
-        status_code = 200
+@shooter_api.route('/', methods=['GET'])
+def get_servers():
+    """Retrieve all existing servers from database."""
+    servers = db.session.query(Server).all()
+    response = servers_schema.dump(servers)
 
-    else:
-        json_data = request.get_json()
-        if not json_data:
-            return jsonify({'error': 'No input data provided.'}), 400
-        # Validate and deserialize input
-        try:
-            data = server_schema.load(json_data).data
-        except ValidationError as err:
-            return jsonify(err.messages), 400
+    return jsonify(response.data), 200
 
-        server = Server.query.filter_by(endpoint=data.get('endpoint')).first()
-        if server:
-            return jsonify({'error': 'Server with this endpoint already exists.'}), 400
 
-        # Create a new server instance
-        server = Server(data)
-        server.save()
+@shooter_api.route('/', methods=['POST'])
+def create_server():
+    """Create new server instance."""
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'error': 'No input data provided.'}), 400
+    # Validate and deserialize input
+    try:
+        data = server_schema.load(json_data).data
+    except ValidationError as err:
+        return jsonify(err.messages), 400
 
-        response = server_schema.dump(server)
-        status_code = 201
+    server = db.session.query.filter(Server.endpoint == data.get('endpoint')).first()
+    if server:
+        return jsonify({'error': 'Server with this endpoint already exists.'}), 400
 
-    return jsonify(response.data), status_code
+    # Create a new server instance
+    server = Server(data)
+    db.session.add(server)
+    db.session.commit()
+
+    response = server_schema.dump(server)
+
+    return jsonify(response.data), 201
 
 
 @shooter_api.route('/<string:endpoint>', methods=['GET', 'PATCH'])
 def get_or_update_server(endpoint):
-    """Return single server instance with endpoint=`endpoint`."""
-    server = Server.query.filter_by(endpoint=endpoint).first()  # TODO: get or 404
+    """Retrieve or update single server instance in database."""
+    server = db.session.query.filter(Server.endpoint == endpoint).first()  # TODO: get or 404
     if server is None:
         return jsonify({'message': 'Server instance could not be found.'}), 404
 
