0eab01d1c0cc0c55240644651ce7debb45919216
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-09-10 20:11:17 +0300

    Fix next/prev values

--- src/app/utils.py
+++ src/app/utils.py
@@ -1,21 +1,20 @@
 """Collections of helpers functions."""
 
-from flask import url_for
+from flask import request
 
 
 def paginate_response(data, page=1):
     """Return formatted responses with pagination data."""
-    # TODO:
     pagination = data.paginate(page, per_page=25, error_out=False)
     prev = None
     if pagination.has_prev:
-        prev = url_for('shooter.get_servers', page=page - 1)
+        prev = f'{request.path}?page={page-1}'
     next = None
     if pagination.has_next:
-        next = url_for('shooter.get_servers', page=page + 1)
+        next = f'{request.path}?page={page+1}'
 
     response = {
-        'servers': [obj.to_dict() for obj in pagination.items],
+        'objects': [obj.to_dict() for obj in pagination.items],
         'prev': prev,
         'next': next,
         'count': pagination.total
